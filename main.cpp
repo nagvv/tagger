55c2b33639f38362b6d22d81875f4753feb68066
Alexey <nagvv97@gmail.com>
2019-01-18 15:21:50 +0300

    integrated sqlite

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <set>
+#include "sqlite3.h"
 
 namespace fs = std::filesystem;
 
@@ -16,15 +17,15 @@ using std::string;
 
 path currentDir;
 
-std::set<path> existFiles;
-std::set<path> newFiles;
-std::set<path> removedFiles;
+set<path> existFiles;
+set<path> newFiles;
+set<path> removedFiles;
 
 void scanForNewFiles()
 {
     std::set<path> allFiles;
     for (const auto &entry : fs::directory_iterator(currentDir))
-        allFiles.insert( fs::canonical( entry.path() ) );
+        allFiles.insert( fs::canonical( entry.path() ) ); //TODO: add ignoring db file
 
     for ( const auto &path : allFiles )
         if ( existFiles.find(path) == existFiles.end() )
@@ -56,7 +57,7 @@ int getChoice()
             "\t1) Tag new files\n"
             "\t2) Edit tags.\n"
             "\t3) Manage tags.\n"
-            "\t4) do not choose this.\n" << endl;
+            "\t4) Search.\n" << endl;
     int c;
     auto getStr = []()
     {
@@ -80,13 +81,77 @@ int getChoice()
     return c;
 }
 
+//removes spaces in start and end, and replaces multiple space with one
+string clearTag( string dirty )
+{
+    string ret;
+    bool ws = true;
+    for ( auto &c : dirty )
+    {
+        if( std::isspace(c) )
+        {
+            if( ws )
+                continue;
+            else
+                ws = true;
+        }
+        else
+        {
+            ws = false;
+            ret.push_back(c);
+        }
+    }
+    if( std::isspace( ret.back() ) )
+        ret.pop_back();
+    return ret;
+}
+
+void tagNewFiles()
+{
+    cin.clear(); cin.ignore();
+    for ( const auto &file : newFiles )
+    {
+        string in; set<string> tags;
+        cout << "Set tags for file: " << file.filename() << " separated by commas." << endl;
+        std::getline(cin, in);
+        std::stringstream ss(in);
+        while ( ss.good() )
+        {
+            string tag;
+            getline( ss, tag, ',' );
+            tag = clearTag(tag);
+            if ( !tag.empty() )
+                tags.insert(tag);
+        }
+        //TODO: insert file and tags to db
+        //TODO: add the possibility to stop tagging
+    }
+}
+
+//TODO: add russian letters support
 int main() {
     currentDir = fs::current_path();//TODO: add try-catch or pass error_code?
     //existFiles - load from DB
+    sqlite3 *db = 0;
+    char *err = 0;
+    if ( sqlite3_open("tags.db", &db) )
+        cout << "Couldn't open/create database: " << sqlite3_errmsg(db) << endl;
+
+    //sqlite3_exec(db, "zapros", 0, 0, &err)
+
     scanForNewFiles();
     while ( int c = getChoice() )
     {
-        cout << "choo-choo" << endl;
+        switch(c)
+        {
+            case 1:
+                tagNewFiles();
+                break;
+            default:
+                cout << "Wrong number." <<endl;
+        }
     }
+
+    sqlite3_close(db);
     return 0;
 }
